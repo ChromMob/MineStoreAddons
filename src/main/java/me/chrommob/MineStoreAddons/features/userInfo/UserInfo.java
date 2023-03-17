73ec4ef36a5cef131e8919ac23a90dd386c88e97
chrommob <filip.chromy04@gmail.com>
2023-03-17 09:51:57 +0100

    Remove debug and add configurable message

--- src/main/java/me/chrommob/MineStoreAddons/features/userInfo/UserInfo.java
+++ src/main/java/me/chrommob/MineStoreAddons/features/userInfo/UserInfo.java
@@ -5,8 +5,11 @@ import me.chrommob.MineStoreAddons.MineStoreAddonsMain;
 import me.chrommob.MineStoreAddons.socket.SocketResponse;
 import me.chrommob.minestore.common.MineStoreCommon;
 
+import java.util.Map;
+
 public class UserInfo implements SocketResponse {
     private final MineStoreAddonsMain main;
+    private String notFound;
     private final Gson gs = new Gson();
     public UserInfo(MineStoreAddonsMain main) {
         this.main = main;
@@ -14,6 +17,8 @@ public class UserInfo implements SocketResponse {
     }
 
     private void init() {
+        Map<String, Object> userInfo = (Map<String, Object>) main.getConfig().get("user-info");
+        notFound = (String) userInfo.get("not-found");
         main.registerSocketResponse(this);
         main.getCommon().commandManager().registerCommand(new Command(this));
     }
@@ -28,13 +33,16 @@ public class UserInfo implements SocketResponse {
         if (!split[0].equalsIgnoreCase("userinfo")) {
             return;
         }
-        MineStoreCommon.getInstance().log("Received userinfo response: " + split[1]);
-        InfoResponse infoResponse = gs.fromJson(split[1], InfoResponse.class);
+        InfoResponse infoResponse = gs.fromJson(response.replace(split[0] + "-", ""), InfoResponse.class);
         if (infoResponse == null) {
             MineStoreCommon.getInstance().log("Error while parsing userinfo response");
             return;
         }
-        main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(infoResponse.getUsername() + "paid " + infoResponse.getAmount() + " for:");
+        if (infoResponse.getPackageAmount().isEmpty()) {
+            main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(main.getCommon().miniMessage().deserialize(notFound.replaceAll("%player%", infoResponse.getUsername())));
+            return;
+        }
+        main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(infoResponse.getUsername() + " paid " + infoResponse.getAmount() + " for:");
         infoResponse.getPackageAmount().forEach((packageName, amount) -> {
             main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(packageName + " x" + amount + " for " + infoResponse.getPackagePrice().get(packageName));
         });
