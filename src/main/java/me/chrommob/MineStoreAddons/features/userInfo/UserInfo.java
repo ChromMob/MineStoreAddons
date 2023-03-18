938965a9096d00c53e428fb7263126473ab377ea
chrommob <filip.chromy04@gmail.com>
2023-03-18 07:51:06 +0100

    finish the new feature

--- src/main/java/me/chrommob/MineStoreAddons/features/userInfo/UserInfo.java
+++ src/main/java/me/chrommob/MineStoreAddons/features/userInfo/UserInfo.java
@@ -4,12 +4,19 @@ import com.google.gson.Gson;
 import me.chrommob.MineStoreAddons.MineStoreAddonsMain;
 import me.chrommob.MineStoreAddons.socket.SocketResponse;
 import me.chrommob.minestore.common.MineStoreCommon;
+import me.chrommob.minestore.common.interfaces.gui.CommonInventory;
+import me.chrommob.minestore.common.interfaces.gui.CommonItem;
+import net.kyori.adventure.text.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class UserInfo implements SocketResponse {
     private final MineStoreAddonsMain main;
     private String notFound;
+    private String guiName;
+    private String item;
     private final Gson gs = new Gson();
     public UserInfo(MineStoreAddonsMain main) {
         this.main = main;
@@ -19,6 +26,9 @@ public class UserInfo implements SocketResponse {
     private void init() {
         Map<String, Object> userInfo = (Map<String, Object>) main.getConfig().get("user-info");
         notFound = (String) userInfo.get("not-found");
+        Map<String, Object> gui = (Map<String, Object>) userInfo.get("gui");
+        guiName = (String) gui.get("title");
+        item = (String) gui.get("item");
         main.registerSocketResponse(this);
         main.getCommon().commandManager().registerCommand(new Command(this));
     }
@@ -42,9 +52,16 @@ public class UserInfo implements SocketResponse {
             main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(main.getCommon().miniMessage().deserialize(notFound.replaceAll("%player%", infoResponse.getUsername())));
             return;
         }
-        main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(infoResponse.getUsername() + " paid " + infoResponse.getAmount() + " for:");
-        infoResponse.getPackageAmount().forEach((packageName, amount) -> {
-            main.getCommon().userGetter().get(infoResponse.getSender()).sendMessage(packageName + " x" + amount + " for " + infoResponse.getPackagePrice().get(packageName));
-        });
+        List<CommonItem> items = new ArrayList<>();
+        for (String name : infoResponse.getPackageAmount().keySet()) {
+            List<Component> lore = new ArrayList<>();
+            lore.add(Component.text("Amount: " + infoResponse.getPackageAmount().get(name)));
+            lore.add(Component.text("Price: " + infoResponse.getPackagePrice().get(name)));
+            items.add(new CommonItem(Component.text(name), item, lore));
+        }
+        Component title = main.getCommon().miniMessage().deserialize(guiName.replaceAll("%player%", infoResponse.getUsername()));
+        main.getCommon().guiData().getGuiInfo().getTitles().add(title);
+        CommonInventory inventory =  new CommonInventory(title, 54, items);
+        main.getCommon().runOnMainThread(() -> main.getCommon().userGetter().get(infoResponse.getSender()).openInventory(inventory));
     }
 }
